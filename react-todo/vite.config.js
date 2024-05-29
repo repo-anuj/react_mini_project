2e28f60b06678c170dcba8ad61513b94611ce7ce
Anuj Dubey <119771415+repo-anuj@users.noreply.github.com>
2024-05-29 12:04:02 +0000

    deploing

--- react-todo/vite.config.js
+++ react-todo/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react_mini_project', // Adjust this to reflect your repository structure
+  base: '/react_mini_project/react-todo', // Adjust this to reflect your repository structure
 });
